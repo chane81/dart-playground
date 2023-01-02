ffc3510507b8d4a5d3c9d4beaa96c29fa690e7fc
chane81 <chane81@naver.com>
2023-01-03 00:15:39 +0900

    feat: list

---
+++ 06. list.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  bool isValid = true;
+  List<int> numbers = [
+    1,
+    2,
+    3,
+    4,
+
+    // coll ection if
+    if (isValid) 5,
+  ];
+
+  numbers.add(6);
+
+  print(numbers);
+}
